66bfbfd37e1af5198d87221d06c7b2357e788534
Diaser8 <diaser1987@gmail.com>
2024-05-11 20:03:22 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+my_list = ['banana', 'apple', 'melon', 'orange']
+print( my_list )
+print (my_list[0])
+print ( my_list [-1])
+print ( my_list [2:3])
+my_dict = { 'banana': 'банан', 'apple': 'яблоко', 'melon': 'дыня', 'orange': 'апельсин'}
+print (my_dict)
+print(my_dict ['banana'])
+my_dict ['banana'] = 'плохой банан'
+print (my_dict)
